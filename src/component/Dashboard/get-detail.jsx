a44f8bf48bc52a3e9d9d45f16f38c9f0b2a0637a
techvogue <vishwagautam57.com>
2024-06-15 20:20:29 +0530

    Updated project with new features

--- src/component/Dashboard/get-detail.jsx
+++ src/component/Dashboard/get-detail.jsx
@@ -64,7 +64,16 @@ export default function Coins() {
 
     fetchData();
   }, []); // Empty dependency array ensures useEffect runs only once
-
+  if (loading) {
+    return (
+      <div className="flex w-full space-x-2 justify-center items-center bg-white h-screen dark:invert">
+        <span className="sr-only">Loading...</span>
+        <div className="h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+        <div className="h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+        <div className="h-8 w-8 bg-black rounded-full animate-bounce"></div>
+      </div>
+    );
+  }
   return (
     <div>
       <form className="max-w-3xl mx-auto mb-8">
